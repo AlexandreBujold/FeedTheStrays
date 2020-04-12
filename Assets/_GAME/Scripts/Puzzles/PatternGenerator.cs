d0e9e6d54b561fd6f61daf7f9967ec7453ed8fa7
Loims <34040415+Loims@users.noreply.github.com>
2020-04-11 20:11:16 -0400

    Add pattern generator
    - Non-monobehaviour static pattern generator
    - Generates a random list of pattern colors
    - List used to determine what pattern players must replicate

---
+++ Assets/_GAME/Scripts/Puzzles/PatternGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PatternGenerator
+{
+    private static int colorEnumLength = (int)PuzzleColors.LengthOperator;
+
+    //private void OnEnable()
+    //{
+    //    colorEnumLength = (int)PuzzleColors.LengthOperator; //Length
+    //}
+
+    public static PuzzleColors[] GeneratePattern(int length)
+    {
+        Debug.Log("Called");
+        PuzzleColors[] newPattern = new PuzzleColors[length];
+
+        for(int i=0;i<length;i++)
+        {
+            PuzzleColors color = (PuzzleColors)Random.Range(0, colorEnumLength);
+            newPattern[i] = color;
+        }
+
+        return newPattern;
+    }
+}
---
+++ Assets/_GAME/Scripts/Puzzles/PuzzleManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public static PuzzleManager instance;
+
+    [Header("Pattern Length")]
+    [SerializeField] private int patternLength; //Length of the pattern
+
+    [Space]
+    [Header("Matched pattern bool")]
+    [SerializeField] private bool playerMatchPattern = false; //Boolean to determine if any player has matched the pattern
+
+    [Space]
+    [Header("Pattern")]
+    [SerializeField] public PuzzleColors[] pattern; //The list that will contain the pattern
+
+    public UnityEvent patternEvent;
+
+    private void OnEnable()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        InitializePattern(patternLength);
+    }
+
+    private IEnumerator WaitForPatternMatch()
+    {
+        yield return new WaitUntil(() => playerMatchPattern == true);
+        //Logic here for progressing stuff
+        yield break;
+    }
+
+    public void InitializePattern(int length)
+    {
+        pattern = PatternGenerator.GeneratePattern(length);
+    }
+}
