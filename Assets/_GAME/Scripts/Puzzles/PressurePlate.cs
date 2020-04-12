0fab40455f33aed7abcea4e71892565365a769c3
Loims <34040415+Loims@users.noreply.github.com>
2020-04-11 23:45:01 -0400

    Pushed in progress stuff

---
+++ Assets/_GAME/Scripts/Puzzles/PressurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] private PuzzleColors color;
+
+    private PuzzleMatcher matcherInstance;
+
+    public UnityEvent<PuzzleColors, GameObject> onPressed;
+
+    private void Awake()
+    {
+        matcherInstance = PuzzleMatcher.instance;
+    }
+
+    private void Start()
+    {
+        onPressed.AddListener(matcherInstance.AddValueToList);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //This will be changed when players are in. Right now it collides with everythings
+        onPressed.Invoke(color, other.gameObject);
+    }
+}
---
+++ Assets/_GAME/Scripts/Puzzles/PuzzleMatcher.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PuzzleMatcher : MonoBehaviour
+{
+    public static PuzzleMatcher instance;
+
+    [Header("Puzzle Manager Instance")]
+    [SerializeField] private PuzzleManager manager;
+
+    [Space]
+    [Header("Player References")]
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    [Space]
+    [Header("Player Pattern Variables")]
+    [SerializeField] private int patternLength;
+    [SerializeField] private PuzzleColors[] player1Pattern;
+    [SerializeField] private PuzzleColors[] player2Pattern;
+    [SerializeField] private int player1Increment = 0;
+    [SerializeField] private int player2Increment = 0;
+
+    private void OnEnable()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        manager = PuzzleManager.instance;
+    }
+
+    public void CreateSizedLists()
+    {
+        patternLength = manager.GetPatternLength();
+
+        player1Pattern = new PuzzleColors[patternLength];
+        player2Pattern = new PuzzleColors[patternLength];
+    }
+
+    public void AddValueToList(PuzzleColors color, GameObject player)
+    {
+        if(player == player1)
+        {
+            player1Pattern[player1Increment] = color;
+            player1Increment++;
+            
+            if(player1Increment == patternLength - 1)
+            {
+                CheckPatternMatch(player1Pattern, player);
+            }
+        }
+        else if(player == player2)
+        {
+            player2Pattern[player2Increment] = color;
+            player2Increment++;
+
+            if(player2Increment == patternLength - 1)
+            {
+                CheckPatternMatch(player2Pattern, player);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Object not of type Player. Functionality skipped");
+        }
+    }
+
+    private void CheckPatternMatch(PuzzleColors[] pattern, GameObject player)
+    {
+        if(player == player1)
+        {
+            if(Enumerable.SequenceEqual(pattern, manager.pattern))
+            {
+                Debug.Log("Both arrays equal. Give point to player 1");
+                //Do further code here
+            }
+            else
+            {
+                Debug.Log("Both arrays inequal. Resetting player 1 array");
+                Array.Clear(player1Pattern, 0, patternLength);
+                player1Increment = 0;
+            }
+
+        }
+        else if(player == player2)
+        {
+            if (Enumerable.SequenceEqual(pattern, manager.pattern))
+            {
+                Debug.Log("Both arrays equal. Give point to player 2");
+                //Do further code here
+            }
+            else
+            {
+                Debug.Log("Both arrays inequal. Resetting player 2 array");
+                Array.Clear(player2Pattern, 0, patternLength);
+                player2Increment = 0;
+            }
+        }
+
+    }
+}
